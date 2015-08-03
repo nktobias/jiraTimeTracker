6cd9209dc153ef27022c48e52a2612ea0ebf68ed
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-03 21:13:16 +0200

    skeleton of local database

---
+++ JiraTimeTracker/LocalDataStorage/IssueEntity.cs
@@ -0,0 +1,12 @@
+﻿using SQLite.Net.Attributes;
+
+namespace Triosoft.JiraTimeTracker.LocalDataStorage
+{
+   public class IssueEntity
+   {
+      [PrimaryKey]
+      public string Key { get; set; }
+      public string IssueType { get; set; }
+      public string Summary { get; set; }
+   }
+}
---
+++ JiraTimeTracker/LocalDataStorage/SqliteDataStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Triosoft.JiraTimeTracker.LocalDataStorage
+{
+   public class SqliteDataStorage
+   {
+      public IEnumerable<Issue> GetIssues()
+      {
+         throw new NotImplementedException();
+      }
+
+      public void SetIssues(IEnumerable<Issue> issues)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
