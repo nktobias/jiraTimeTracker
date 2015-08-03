47b8177a3f8d0df3408ed4d88a4c8bc784ec9111
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-03 21:58:58 +0200

    issue manipulation commands

---
+++ JiraTimeTracker/Actions/DownloadIssuessCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Triosoft.JiraTimeTracker.JiraRestApi;
+using Triosoft.JiraTimeTracker.LocalDataStorage;
+
+namespace Triosoft.JiraTimeTracker.Actions
+{
+   public class DownloadIssuessCommand
+   {
+      private readonly JiraApiClient _jiraApiClient;
+      private readonly SqliteDataStorage _sqliteDataStorage = new SqliteDataStorage();
+
+      public DownloadIssuessCommand(JiraApiClient jiraApiClient)
+      {
+         _jiraApiClient = jiraApiClient;
+      }
+
+      public async Task ExecuteAsync()
+      {
+         IEnumerable<Issue> downloadedIssues = await _jiraApiClient.GetIssuesAsync();
+         _sqliteDataStorage.SetIssues(downloadedIssues);
+      }
+   }
+}
---
+++ JiraTimeTracker/Actions/GetLocalIssuesQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Triosoft.JiraTimeTracker.LocalDataStorage;
+
+namespace Triosoft.JiraTimeTracker.Actions
+{
+   public class GetLocalIssuesQuery
+   {
+      private readonly SqliteDataStorage _sqliteDataStorage = new SqliteDataStorage();
+
+      public IEnumerable<Issue> Execute()
+      {
+         return _sqliteDataStorage.GetIssues();
+      }       
+   }
+}
