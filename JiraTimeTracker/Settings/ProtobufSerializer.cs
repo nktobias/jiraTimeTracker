e446e6f0588eec2cde61f5d5444542aeac2836ae
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-03 17:47:15 +0200

    added missing ProtobufSerializer.cs

---
+++ JiraTimeTracker/Settings/ProtobufSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using ProtoBuf;
+
+namespace Triosoft.JiraTimeTracker.Settings
+{
+   public class ProtobufSerializer
+   {
+      public byte[] Serialize<TData, TContract>(TData input)
+         where TContract : IProtobufContract<TData>, new()
+      {
+         byte[] result;
+
+         using (MemoryStream serializationStream = new MemoryStream())
+         {
+            TContract contract = new TContract();
+            contract.InitializeFromDataObject(input);
+
+            Serializer.Serialize(serializationStream, contract);
+            result = serializationStream.ToArray();
+         }
+
+         return result;
+      }
+
+      public TData Deserialize<TData, TContract>(byte[] input)
+         where TContract : IProtobufContract<TData>
+      {
+         TData result;
+
+         using (MemoryStream deserializationStream = new MemoryStream(input))
+         {
+            TContract contract = Serializer.Deserialize<TContract>(deserializationStream);
+            result = contract.ToDataObject();
+         }
+
+         return result;
+      }
+   }
+}
