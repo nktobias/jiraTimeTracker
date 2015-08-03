c4ba3b678419e68e535e46967c455d7137be22d0
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-03 21:47:10 +0200

    local database for issues

--- JiraTimeTracker/LocalDataStorage/SqliteDataStorage.cs
+++ JiraTimeTracker/LocalDataStorage/SqliteDataStorage.cs
@@ -1,18 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using SQLite.Net;
+using SQLite.Net.Platform.Win32;
 
 namespace Triosoft.JiraTimeTracker.LocalDataStorage
 {
    public class SqliteDataStorage
    {
+      private const string DatabaseFileName = "3jtt.db";
+
+      private readonly ApplicationStorageFolder _applicationStorageFolder = new ApplicationStorageFolder();
+      private readonly SQLiteConnection _connection;
+
+      public SqliteDataStorage()
+      {
+         bool createTables = !_applicationStorageFolder.FileExists(DatabaseFileName);
+
+         _connection = new SQLiteConnection(new SQLitePlatformWin32(), ApplicationStorageFolder.GetPathToFileInDirectory(DatabaseFileName));
+         if (createTables)
+         {
+            _connection.CreateTable<IssueEntity>();
+         }
+      }
+
       public IEnumerable<Issue> GetIssues()
       {
-         throw new NotImplementedException();
+         return _connection.Table<IssueEntity>().Select(x => new Issue(x.Key, x.IssueType, x.Summary));
       }
 
       public void SetIssues(IEnumerable<Issue> issues)
       {
-         throw new NotImplementedException();
+         _connection.BeginTransaction();
+         _connection.DeleteAll<IssueEntity>();
+         _connection.InsertAll(issues);
+         _connection.Commit();
       }
    }
 }
