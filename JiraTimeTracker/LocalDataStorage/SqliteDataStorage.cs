838ee2ec451ba0bf4249e9327c9456982ecff8a7
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-05 12:35:53 +0200

    rewrite to System.Data.Sqlite

--- JiraTimeTracker/LocalDataStorage/SqliteDataStorage.cs
+++ JiraTimeTracker/LocalDataStorage/SqliteDataStorage.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using Triosoft.JiraTimeTracker.WorkLogging;
 
@@ -16,34 +17,97 @@ namespace Triosoft.JiraTimeTracker.LocalDataStorage
          bool createTables = !_applicationStorageFolder.FileExists(DatabaseFileName);
 
          _connection = new SQLiteConnection(string.Format("Data Source={0};Version=3", ApplicationStorageFolder.GetPathToFileInDirectory(DatabaseFileName)));
+         _connection.Open();
          if (createTables)
          {
-            using (SQLiteCommand command = _connection.CreateCommand())
-            {
-               command.CommandText = "CREATE TABLE Issue(Key TEXT PRIMARY KEY NOT NULL, Type TEXT NOT NULL, Summary TEXT NOT NULL)";
-               command.ExecuteNonQuery();
-            }
+            ExecuteNonQuery("CREATE TABLE Issue(Key varchar PRIMARY KEY NOT NULL, Type varchar NOT NULL, Summary varchar NOT NULL)");
+            ExecuteNonQuery("CREATE TABLE Worklog(Id integer PRIMARY KEY AUTOINCREMENT NOT NULL, IssueKey varchar NOT NULL, Start bigint NOT NULL, DurationInSeconds bigint NOT NULL)");
          }
       }
 
       public IEnumerable<Issue> GetIssues()
       {
-         return null;
+         using (SQLiteCommand command = _connection.CreateCommand())
+         {
+            command.CommandText = "SELECT Key, Type, Summary FROM Issue";
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+               while (reader.Read())
+               {
+                  yield return new Issue(
+                     (string)reader["Key"],
+                     (string)reader["Type"],
+                     (string)reader["Summary"]);
+               }
+            }
+         }
       }
 
       public void SetIssues(IEnumerable<Issue> issues)
       {
-         
+         using (SQLiteTransaction transaction = _connection.BeginTransaction())
+         {
+            ExecuteNonQuery("DELETE FROM Issue", transaction);
+            foreach (Issue issue in issues)
+            {
+               using (SQLiteCommand command = _connection.CreateCommand())
+               {
+                  command.Transaction = transaction;
+                  command.CommandText = "INSERT INTO Issue (Key, Type, Summary) VALUES (@Key, @Type, @Summary)";
+                  command.Parameters.AddWithValue("@Key", issue.Key);
+                  command.Parameters.AddWithValue("@Type", issue.Type);
+                  command.Parameters.AddWithValue("@Summary", issue.Summary);
+                  command.ExecuteNonQuery();
+               }
+            }
+
+            transaction.Commit();
+         }
       }
 
       public void AddWorklog(Worklog worklog)
       {
-         
+         using (SQLiteCommand command = _connection.CreateCommand())
+         {
+            command.CommandText = "INSERT INTO Worklog (IssueKey, Start, DurationInSeconds) VALUES (@IssueKey, @Start, @DurationInSeconds)";
+            command.Parameters.AddWithValue("@IssueKey", worklog.Issue.Key);
+            command.Parameters.AddWithValue("@Start", worklog.Start.Ticks);
+            command.Parameters.AddWithValue("@DurationInSeconds", worklog.DurationInSeconds);
+            command.ExecuteNonQuery();
+         }
       }
 
       public IEnumerable<Worklog> GetWorklogs()
       {
-         return null;
+         using (SQLiteCommand command = _connection.CreateCommand())
+         {
+            command.CommandText = "SELECT I.Key AS IssueKey, I.Type AS IssueType, I.Summary AS IssueSummary, W.Start AS WorklogStart, W.DurationInSeconds AS WorklogDurationInSeconds FROM Issue I INNER JOIN Worklog W ON I.Key = W.IssueKey";
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+               while (reader.Read())
+               {
+                  Issue issue = new Issue(
+                     (string)reader["IssueKey"],
+                     (string)reader["IssueType"],
+                     (string)reader["IssueSummary"]);
+
+                  yield return new Worklog(
+                     issue, 
+                     new DateTime((long)reader["WorklogStart"]),
+                     TimeSpan.FromSeconds((long)reader["WorklogDurationInSeconds"]));
+               }
+            }
+         }
+      }
+
+      private void ExecuteNonQuery(string queryText, SQLiteTransaction transaction = null)
+      {
+         using (SQLiteCommand command = _connection.CreateCommand())
+         {
+            command.Transaction = transaction;
+            command.CommandText = queryText;
+            command.ExecuteNonQuery();
+         }
       }
    }
 }
