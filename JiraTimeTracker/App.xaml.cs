f08697a6cf4d66f4acfcdecd4ccdfab28fece135
Nikola Anusev <nikola.anusev@3osoft.com>
2015-08-06 15:58:54 +0200

    basic error handling

--- JiraTimeTracker/App.xaml.cs
+++ JiraTimeTracker/App.xaml.cs
@@ -1,6 +1,21 @@
-﻿namespace Triosoft.JiraTimeTracker
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace Triosoft.JiraTimeTracker
 {
    public partial class App
    {
+      protected override void OnStartup(StartupEventArgs e)
+      {
+         base.OnStartup(e);
+
+         DispatcherUnhandledException += HandleAppDispatcherUnhandledException;
+      }
+
+      private void HandleAppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+      {
+         MessageBox.Show("There was an error: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+         e.Handled = true;
+      }
    }
 }
--- JiraTimeTracker/JiraRestApi/RestSharpHttpException.cs
+++ JiraTimeTracker/JiraRestApi/RestSharpHttpException.cs
@@ -6,11 +6,27 @@ namespace Triosoft.JiraTimeTracker.JiraRestApi
    public class RestSharpHttpException : Exception
    {
       public RestSharpHttpException(IRestResponse response) : base(
-         string.Format("There was an HTTP problem. Code: {0}, Message: {1}", response.StatusCode, response.Content))
+         GetMessageFromResponse(response))
       {
          Response = response;
       }
 
       public IRestResponse Response { get; private set; }
+
+      private static string GetMessageFromResponse(IRestResponse response)
+      {
+         string result;
+
+         if (response.StatusCode == 0)
+         {
+            result = string.Format("There was an HTTP problem: {0}", response.ErrorMessage);
+         }
+         else
+         {
+            result = string.Format("There was an HTTP problem. Code: {0}, Message: {1}", response.StatusCode, response.ErrorMessage);
+         }
+
+         return result;
+      }
    }
 }
